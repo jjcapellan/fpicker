fcd97ca4242decc5003f8b7b4ea330bd4eb4e02c
jjcapellan <soycape@hotmail.com>
2023-09-05 12:50:21 +0200

    New feature: support for custom mux

--- fpicker.go
+++ fpicker.go
@@ -27,7 +27,31 @@ func init() {
 	home, _ = fsinfo.GetHomePath()
 	tmpl, _ = initTemplates()
 	currentDir, _ = os.Getwd()
+}
 
-	http.HandleFunc(FilePickerUrl, handleFilePicker)
-	http.HandleFunc(FolderPickerUrl, handleFolderPicker)
+// Setup configures route handlers for file and folder selection functionalities
+// provided by the fpicker package. Call this function to register the corresponding
+// HTTP route handlers either on an existing HTTP multiplexer (http.ServeMux) or, if
+// 'mux' is nil, directly on Go's global HTTP server (http.DefaultServeMux).
+//
+// When users make GET requests to the registered URLs, such as FilePickerUrl and
+// FolderPickerUrl, they can retrieve the respective HTML content for file and folder
+// pickers.
+//
+// Example usage:
+//   mux := http.NewServeMux()
+//   fpicker.Setup(mux)
+//   http.ListenAndServe(":8080", mux)
+//
+// Parameters:
+//   mux: The HTTP multiplexer where route handlers will be registered. If nil, the
+//        handlers will be registered on http.DefaultServeMux.
+func Setup(mux *http.ServeMux) {
+	if mux == nil {
+		http.HandleFunc(FilePickerUrl, handleFilePicker)
+		http.HandleFunc(FolderPickerUrl, handleFolderPicker)
+		return
+	}
+	mux.HandleFunc(FilePickerUrl, handleFilePicker)
+	mux.HandleFunc(FolderPickerUrl, handleFolderPicker)
 }
