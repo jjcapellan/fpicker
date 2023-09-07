5d285146befd6831e6945637345d2bc658d5611d
jjcapellan <soycape@hotmail.com>
2023-09-07 22:11:14 +0200

    Docs: css functions

--- fpicker.go
+++ fpicker.go
@@ -30,10 +30,47 @@ func init() {
 	currentDir, _ = os.Getwd()
 }
 
+// ResetCssVars resets CSS variables to default values.
 func ResetCssVars() {
 	cssVars = ""
 }
 
+// SetCssVars allows you to override default CSS variables within the template.
+// In this way you can customize the color scheme.
+//
+// Parameters:
+//   vars (string): A string containing CSS variable declarations with the format
+//                 "--variable-name: value;", where "variable-name" is the name of the
+//                 CSS variable to override, and "value" is the new value to set.
+//
+// These are the default css variables:
+//   --color-dark: #2a363c;
+//
+//   /* Body */
+//   --background-color: #a2a2a2;
+//
+//   /* Content */
+//   --background-color-content: #f6f8f3;
+//   --color-content: var(--color-dark);
+//
+//   /* Sidebar */
+//   --background-color-sidebar: #e1e9ec;
+//   --color-sidebar: var(--color-dark);
+//   --color-sidebar-hover: #c51818;
+//
+//   /* Bars */
+//   --background-color-bar: #546c78;
+//   --color-bar: var(--background-color-sidebar);
+//
+//   /* Buttons */
+//   --background-color-button: var(--color-dark);
+//   --color-button: var(--background-color-sidebar);
+//   --background-color-button-hover: #9fb2bc;
+//   --color-button-hover: var(--color-dark);
+//
+// Example:
+//   fpicker.SetCssVars("--background-color: #cc0000; --color-sidebar-hover: #c50000;")
+//
 func SetCssVars(vars string) {
 	cssVars = vars
 }
